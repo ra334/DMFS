1a0d7f2541e3f94689c1412adf594071a1d33c92
ra334 <savcukmihajlo@gmail.com>
2022-09-23 13:08:11 +0300

    beta

--- mp3-converter.js
+++ mp3-converter.js
@@ -3,18 +3,21 @@ const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 
-const arr = fs.readFileSync('music_link.txt').toString().split('\n');
+const music_arr = fs.readFileSync('music_link.txt').toString().split('\n');
+let count = 0
 
-async function converter(music_link) {
+
+async function converter(music_link, music_name) {
 
     let stream = ytdl(music_link, {
         quality: 'highestaudio',
     });
 
     let start = Date.now();
+    console.log(music_link, music_name)
     await ffmpeg(stream)
         .audioBitrate(128)
-        .save(`${__dirname}/${music_link.replaceAll('+', ' ')}.mp3`)
+        .save(`${__dirname}/${music_name}.mp3`)
         .on('progress', p => {
             readline.cursorTo(process.stdout, 0);
             process.stdout.write(`${p.targetSize}kb downloaded`);
@@ -26,9 +29,10 @@ async function converter(music_link) {
 
 
 async function start() {
-    for (let i of arr) {
-        await converter(i)
-        console.log(i)
+    for (let i of music_arr) {
+        count += 1
+        console.log(typeof i)
+        await converter(i, count)
     }
 }
 
